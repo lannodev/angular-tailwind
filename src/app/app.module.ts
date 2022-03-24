f6996c73d27c9abbc163dfa16fe318cd278b80b1
Luciano Oliveira <luciano.oliveira@ancora.com.br>
2022-03-24 17:52:21 -0300

    feat: added layout structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/admin/sidebar/sidebar.component';
+import { NavbarComponent } from './layout/admin/navbar/navbar.component';
+import { FooterComponent } from './layout/admin/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
