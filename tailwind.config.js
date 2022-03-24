338d410cde0a76a12fb2f174b0286aed572a3141
Luciano Oliveira <luciano.oliveira@ancora.com.br>
2022-03-24 15:05:24 -0300

    feat: initial commit 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: '',
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
