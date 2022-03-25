eed7bd3f40d8f9e0956ca58951acfe877a68614a
Luciano Oliveira <luciano.oliveira@ancora.com.br>
2022-03-25 16:46:21 -0300

    feat: change font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito Sans', 'sans-serif'],
+      }
+    },
   },
   variants: {
     extend: {},
