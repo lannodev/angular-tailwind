951d8280119a9e230c189d4688660cfdf56493fb
lubisk8 <lubisk8@hotmail.com>
2022-03-30 20:41:41 -0300

    refactor: change color dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,21 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: {
-      'primary': '#1fb6ff',
-      'secondary': '#ff49db'
+    extend: {
+      colors: {
+        night: {
+          50: '#e4e4eb',
+          100: '#bbbace',
+          200: '#8f8ead',
+          300: '#66658c',
+          400: '#4b4777',
+          500: '#302a62',
+          600: '#2b245b',
+          700: '#241c51',
+          800: '#1c1445',
+          900: '#130030',
+        },
+      }
     },
     fontFamily: {
       poppins: ['Poppins', 'system-ui', 'sans-serif'],
