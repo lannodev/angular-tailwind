fbca2e95136163c0ed9cf6000393b892e286b702
Luciano Oliveira <luciano.oliveira@ancora.com.br>
2022-04-01 09:19:45 -0300

    refactor: navbar menu colors

--- src/app/shared/constants/menu.ts
+++ src/app/shared/constants/menu.ts
@@ -7,9 +7,9 @@ export class Menu {
 			group: 'Base',
 			separator: false,
 			items: [
-				{ icon: 'assets/icons/chart-pie.svg', label: 'Dashboard', route: '/dashboard', expanded: false, },
+				{ icon: 'assets/icons/outline/chart-pie.svg', label: 'Dashboard', route: '/dashboard', expanded: false, },
 				{
-					icon: 'assets/icons/bookmark.svg', label: 'Books', route: '/books', expanded: false,
+					icon: 'assets/icons/outline/bookmark.svg', label: 'Books', route: '/books', expanded: false,
 					children: [
 						{ label: 'Current Files', route: '/books/current-files' },
 						{ label: 'Downloads', route: '/books/download' },
@@ -22,19 +22,19 @@ export class Menu {
 			group: 'Colaboration',
 			separator: true,
 			items: [
-				{ icon: 'assets/icons/download.svg', label: 'Download', route: '/download', expanded: false, },
-				{ icon: 'assets/icons/gift.svg', label: 'Gift Card', route: '/gift', expanded: false, },
-				{ icon: 'assets/icons/users.svg', label: 'Users', route: '/users', expanded: false, },
+				{ icon: 'assets/icons/outline/download.svg', label: 'Download', route: '/download', expanded: false, },
+				{ icon: 'assets/icons/outline/gift.svg', label: 'Gift Card', route: '/gift', expanded: false, },
+				{ icon: 'assets/icons/outline/users.svg', label: 'Users', route: '/users', expanded: false, },
 			]
 		},
 		{
 			group: '',
 			separator: false,
 			items: [
-				{ icon: 'assets/icons/cog.svg', label: 'Settings', route: '/settings', expanded: false, },
-				{ icon: 'assets/icons/bell.svg', label: 'Notifications', route: '/gift', expanded: false, },
+				{ icon: 'assets/icons/outline/cog.svg', label: 'Settings', route: '/settings', expanded: false, },
+				{ icon: 'assets/icons/outline/bell.svg', label: 'Notifications', route: '/gift', expanded: false, },
 				{
-					icon: 'assets/icons/folder.svg', label: 'Folders', route: '/folders', expanded: false,
+					icon: 'assets/icons/outline/folder.svg', label: 'Folders', route: '/folders', expanded: false,
 					children: [
 						{ label: 'Current Files', route: '/folders/current-files' },
 						{ label: 'Downloads', route: '/folders/download' },
