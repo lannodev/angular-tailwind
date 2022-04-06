dff6b421a378a102c7cbd04bdc9121212b40c4d3
lubisk8 <12519008+luciano-work@users.noreply.github.com>
2022-04-06 20:53:03 -0300

    refactor: change sidebar menu name and folder

--- src/app/layout/admin/admin.module.ts
+++ src/app/layout/admin/admin.module.ts
@@ -7,7 +7,7 @@ import { PagesRouting } from 'src/app/pages/pages-routing.module';
 import { AdminComponent } from './admin.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarMenuComponent } from './components/sidebar/menu-item/sidebar-menu.component';
+import { SidebarMenuComponent } from './components/sidebar/sidebar-menu/sidebar-menu.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
