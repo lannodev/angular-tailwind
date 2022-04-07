0d1881b187c5129a8c1d069dae2a5bd118e22d92
lubisk8 <12519008+luciano-work@users.noreply.github.com>
2022-04-07 19:28:01 -0300

    feat: add tailwind animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,59 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-out-down': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)'
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+        },
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-out-up': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)'
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        'fade-in-down': 'fade-in-down 0.3s ease-out',
+        'fade-out-down': 'fade-out-down 0.3s ease-out',
+        'fade-in-up': 'fade-in-up 0.3s ease-out',
+        'fade-out-up': 'fade-out-up 0.3s ease-out'
+      },
       boxShadow: {
         'custom': '0px 0px 50px 0px rgb(82 63 105 / 15%)',
       },
