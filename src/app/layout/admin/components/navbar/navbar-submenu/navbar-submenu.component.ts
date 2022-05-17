ae3117d76f026ad5ef69e416d8d60f40b9e2920d
lubisk8 <12519008+luciano-work@users.noreply.github.com>
2022-05-17 20:30:09 -0300

    refactor: navigation bar

--- src/app/layout/admin/components/navbar/navbar-submenu/navbar-submenu.component.ts
+++ src/app/layout/admin/components/navbar/navbar-submenu/navbar-submenu.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { SubMenuItem } from 'src/app/shared/models/menu.model';
 
 @Component({
-  selector: 'app-navbar-submenu',
+  selector: 'div[navbar-submenu]',
   templateUrl: './navbar-submenu.component.html',
   styleUrls: ['./navbar-submenu.component.scss']
 })
