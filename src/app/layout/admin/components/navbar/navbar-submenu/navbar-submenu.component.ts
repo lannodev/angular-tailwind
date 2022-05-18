10c000d2045352089730d34c7991db9b7f22fd13
lubisk8 <12519008+luciano-work@users.noreply.github.com>
2022-05-18 20:15:28 -0300

    refactor: nav menu

--- src/app/layout/admin/components/navbar/navbar-submenu/navbar-submenu.component.ts
+++ src/app/layout/admin/components/navbar/navbar-submenu/navbar-submenu.component.ts
@@ -10,28 +10,9 @@ export class NavbarSubmenuComponent implements OnInit {
 
   @Input() public submenu = <SubMenuItem>{};
 
-  private showMenuClass = ['scale-100', 'animate-fade-in-up', 'opacity-100', 'pointer-events-auto'];
-  private hideMenuClass = ['scale-95', 'animate-fade-out-down', 'opacity-0', 'pointer-events-none'];
-
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  public mouseEnter(event: any): void {
-    let element = event.target.querySelector('app-navbar-submenu').children[0];
-    if (element) {
-      this.hideMenuClass.forEach(c => element.classList.remove(c));
-      this.showMenuClass.forEach(c => element.classList.add(c));
-    }
-  }
-
-  public mouseLeave(event: any): void {
-    let element = event.target.querySelector('app-navbar-submenu').children[0];
-    if (element) {
-      this.showMenuClass.forEach(c => element.classList.remove(c));
-      this.hideMenuClass.forEach(c => element.classList.add(c));
-    }
   }
 
 }
