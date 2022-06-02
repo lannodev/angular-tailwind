521bc0b211e7b00c7ed66ea3b172f512f48295e4
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2022-06-02 10:11:26 -0300

    feat: add plugin for scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,96 +1,98 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	prefix: '',
-	mode: 'jit',
-	important: false,
-	content: ['./src/**/*.{html,ts}'],
-	darkMode: 'class', // or 'media' or 'class'
-	theme: {
-		extend: {
-			keyframes: {
-				wiggle: {
-					'0%, 100%': { transform: 'rotate(-3deg)' },
-					'50%': { transform: 'rotate(3deg)' },
-				},
-				'fade-in-down': {
-					'0%': {
-						opacity: '0',
-						transform: 'translateY(-10px)',
-					},
-					'100%': {
-						opacity: '1',
-						transform: 'translateY(0)',
-					},
-				},
-				'fade-out-down': {
-					from: {
-						opacity: '1',
-						transform: 'translateY(0px)',
-					},
-					to: {
-						opacity: '0',
-						transform: 'translateY(10px)',
-					},
-				},
-				'fade-in-up': {
-					'0%': {
-						opacity: '0',
-						transform: 'translateY(10px)',
-					},
-					'100%': {
-						opacity: '1',
-						transform: 'translateY(0)',
-					},
-				},
-				'fade-out-up': {
-					from: {
-						opacity: '1',
-						transform: 'translateY(0px)',
-					},
-					to: {
-						opacity: '0',
-						transform: 'translateY(10px)',
-					},
-				},
-			},
-			animation: {
-				wiggle: 'wiggle 1s ease-in-out infinite',
-				'fade-in-down': 'fade-in-down 0.3s ease-out',
-				'fade-out-down': 'fade-out-down 0.3s ease-out',
-				'fade-in-up': 'fade-in-up 0.3s ease-out',
-				'fade-out-up': 'fade-out-up 0.3s ease-out',
-			},
-			boxShadow: {
-				custom: '0px 0px 50px 0px rgb(82 63 105 / 15%)',
-			},
-			colors: {
-				primary: colors.green,
-				night: {
-					50: '#e4e4eb',
-					100: '#bbbace',
-					200: '#8f8ead',
-					300: '#66658c',
-					400: '#4b4777',
-					500: '#302a62',
-					600: '#2b245b',
-					700: '#241c51',
-					800: '#1c1445',
-					900: '#130030',
-				},
-			},
-		},
-		fontFamily: {
-			poppins: ['Poppins', 'system-ui', 'sans-serif'],
-			nunito: ['Nunito Sans', 'sans-serif'],
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/typography'),
-		require('@tailwindcss/aspect-ratio')
-	],
+    prefix: '',
+    mode: 'jit',
+    important: false,
+    content: ['./src/**/*.{html,ts}'],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {
+            keyframes: {
+                wiggle: {
+                    '0%, 100%': { transform: 'rotate(-3deg)' },
+                    '50%': { transform: 'rotate(3deg)' },
+                },
+                'fade-in-down': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(-10px)',
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)',
+                    },
+                },
+                'fade-out-down': {
+                    from: {
+                        opacity: '1',
+                        transform: 'translateY(0px)',
+                    },
+                    to: {
+                        opacity: '0',
+                        transform: 'translateY(10px)',
+                    },
+                },
+                'fade-in-up': {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(10px)',
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)',
+                    },
+                },
+                'fade-out-up': {
+                    from: {
+                        opacity: '1',
+                        transform: 'translateY(0px)',
+                    },
+                    to: {
+                        opacity: '0',
+                        transform: 'translateY(10px)',
+                    },
+                },
+            },
+            animation: {
+                wiggle: 'wiggle 1s ease-in-out infinite',
+                'fade-in-down': 'fade-in-down 0.3s ease-out',
+                'fade-out-down': 'fade-out-down 0.3s ease-out',
+                'fade-in-up': 'fade-in-up 0.3s ease-out',
+                'fade-out-up': 'fade-out-up 0.3s ease-out',
+            },
+            boxShadow: {
+                custom: '0px 0px 50px 0px rgb(82 63 105 / 15%)',
+            },
+            colors: {
+                primary: colors.green,
+                night: {
+                    50: '#e4e4eb',
+                    100: '#bbbace',
+                    200: '#8f8ead',
+                    300: '#66658c',
+                    400: '#4b4777',
+                    500: '#302a62',
+                    600: '#2b245b',
+                    700: '#241c51',
+                    800: '#1c1445',
+                    900: '#130030',
+                },
+            },
+        },
+        fontFamily: {
+            poppins: ['Poppins', 'system-ui', 'sans-serif'],
+            nunito: ['Nunito Sans', 'sans-serif'],
+        },
+    },
+    variants: {
+        extend: {},
+        scrollbar: ['dark', 'rounded']
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+        require('tailwind-scrollbar')
+    ],
 }
