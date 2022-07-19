102387c4c4e7a47c74cab4071368a325ada89e3f
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2022-07-19 09:01:32 -0300

    refactor: components folder name

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,41 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
-    {
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
         path: '',
-        component: AuthComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'sign-in',
-                pathMatch: 'full',
-            },
-            {
-                path: 'sign-in',
-                component: SignInComponent,
-                data: { returnUrl: window.location.pathname },
-            },
-            {
-                path: 'sign-up',
-                component: SignUpComponent,
-            },
-            {
-                path: 'forgot-password',
-                component: ForgotPasswordComponent,
-            },
-            { path: '', redirectTo: 'login', pathMatch: 'full' },
-            { path: '**', redirectTo: 'login', pathMatch: 'full' },
-        ],
-    },
+        redirectTo: 'sign-in',
+        pathMatch: 'full',
+      },
+      {
+        path: 'sign-in',
+        component: SignInComponent,
+        data: { returnUrl: window.location.pathname },
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent,
+      },
+      {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent,
+      },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AuthRoutingModule { }
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,25 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { HttpClientModule } from '@angular/common/http';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 
 @NgModule({
-    declarations: [
-        AuthComponent,
-        SignInComponent,
-        SignUpComponent,
-        ForgotPasswordComponent
-    ],
-    imports: [
-        CommonModule,
-        AuthRoutingModule,
-        HttpClientModule,
-        AngularSvgIconModule.forRoot(),
-    ]
+  declarations: [
+    AuthComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    HttpClientModule,
+    AngularSvgIconModule.forRoot(),
+  ]
 })
 export class AuthModule { }
