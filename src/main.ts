4b89b135f542fccd307d133b8dc42bb13cbf10ef
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2022-09-20 09:02:25 -0300

    feat: add production debug console warning

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,22 @@ import { AppModule } from './app/app.module'
 import { environment } from './environments/environment'
 
 if (environment.production) {
-	enableProdMode()
+    enableProdMode()
+    //show this warning only on prod mode
+    if (window) {
+        selfXSSWarning();
+    }
 }
 
 platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.catch((err) => console.error(err))
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err))
+
+function selfXSSWarning() {
+    setTimeout(() => {
+        console.log('%c** STOP **', 'font-weight:bold; font: 2.5em Arial; color: white; background-color: #e11d48; padding-left: 15px; padding-right: 15px; border-radius: 25px; padding-top: 5px; padding-bottom: 5px;');
+        console.log(
+            `\n%cThis is a browser feature intended for developers. Using this console may allow attackers to impersonate you and steal your information sing an attack called Self-XSS. Do not enter or paste code that you do not understand.`, 'font-weight:bold; font: 2em Arial; color: #e11d48;'
+        );
+    });
+}
