6ffba453f1df4096c687854cdc79f2df0e22768b
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2023-03-23 09:09:30 -0300

    feat: add footer to layout

--- src/app/modules/layout/components/footer/footer.component.ts
+++ src/app/modules/layout/components/footer/footer.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  public year: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }
