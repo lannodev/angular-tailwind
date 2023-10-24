59f5590352497bb4eaae8095693aed331fb12faa
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2023-10-24 10:22:28 -0300

    fix: scroll to top when route change
    To fix this issue it was necessary to detect when the route changed and set the scroll position to 0
    on the main content element.
    
    fix #9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.1.2] - 2023-10-24
+
+### Fixed
+
+- Scroll to top when route change (#9)
+
+## [Unreleased]
+
 ## [0.1.1] - 2023-02-03
 
 ### Fixed
--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet, Event } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
-    selector: 'app-layout',
-    templateUrl: './layout.component.html',
-    styleUrls: ['./layout.component.scss'],
-    standalone: true,
-    imports: [
-        SidebarComponent,
-        NavbarComponent,
-        RouterOutlet,
-        FooterComponent,
-    ],
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+  standalone: true,
+  imports: [SidebarComponent, NavbarComponent, RouterOutlet, FooterComponent],
 })
 export class LayoutComponent implements OnInit {
-  constructor() {}
+  private mainContent: HTMLElement | null = null;
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.mainContent) {
+          this.mainContent!.scrollTop = 0;
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.mainContent = document.getElementById('main-content');
+  }
 }
