9767e9bfc678d5d4565b4ac3c295b8975e7980d1
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2024-01-25 14:54:45 -0300

    fix: submenu position

--- src/app/modules/layout/components/navbar/navbar-submenu/navbar-submenu.component.ts
+++ src/app/modules/layout/components/navbar/navbar-submenu/navbar-submenu.component.ts
@@ -1,27 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { SubMenuItem } from 'src/app/core/models/menu.model';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { RouterLinkActive, RouterLink } from '@angular/router';
 import { NgFor, NgTemplateOutlet, NgIf } from '@angular/common';
 
 @Component({
-    selector: 'div[navbar-submenu]',
-    templateUrl: './navbar-submenu.component.html',
-    styleUrls: ['./navbar-submenu.component.scss'],
-    standalone: true,
-    imports: [
-        NgFor,
-        NgTemplateOutlet,
-        RouterLinkActive,
-        RouterLink,
-        NgIf,
-        AngularSvgIconModule,
-    ],
+  selector: 'div[navbar-submenu]',
+  templateUrl: './navbar-submenu.component.html',
+  styleUrls: ['./navbar-submenu.component.scss'],
+  standalone: true,
+  imports: [NgFor, NgTemplateOutlet, RouterLinkActive, RouterLink, NgIf, AngularSvgIconModule],
 })
 export class NavbarSubmenuComponent implements OnInit {
   @Input() public submenu = <SubMenuItem[]>{};
+  @ViewChild('submenuRef') submenuRef: ElementRef<HTMLDivElement> | undefined;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    /**
+     * check if component is out of the screen
+     */
+    if (this.submenuRef) {
+      const submenu = this.submenuRef.nativeElement.getBoundingClientRect();
+      const bounding = document.body.getBoundingClientRect();
+
+      if (submenu.right > bounding.right) {
+        const childrenElement = this.submenuRef.nativeElement.parentNode as HTMLElement;
+        if (childrenElement) {
+          childrenElement.style.left = '-100%';
+        }
+      }
+    }
+  }
 }
