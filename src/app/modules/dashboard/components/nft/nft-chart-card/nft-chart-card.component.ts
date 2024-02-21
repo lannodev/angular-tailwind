35307e5e861b6a48d2a13825715c66c2f229645c
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2024-02-21 14:37:34 -0300

    refactor: apex chart scss varibles

--- src/app/modules/dashboard/components/nft/nft-chart-card/nft-chart-card.component.ts
+++ src/app/modules/dashboard/components/nft/nft-chart-card/nft-chart-card.component.ts
@@ -6,16 +6,16 @@ import { NgApexchartsModule } from 'ng-apexcharts';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @Component({
-    selector: '[nft-chart-card]',
-    templateUrl: './nft-chart-card.component.html',
-    standalone: true,
-    imports: [AngularSvgIconModule, NgApexchartsModule],
+  selector: '[nft-chart-card]',
+  templateUrl: './nft-chart-card.component.html',
+  standalone: true,
+  imports: [AngularSvgIconModule, NgApexchartsModule],
 })
 export class NftChartCardComponent implements OnInit, OnDestroy {
   public chartOptions: Partial<ChartOptions>;
 
   constructor(private themeService: ThemeService) {
-    const baseColor = '#7239ea';
+    let baseColor = '#FFFFFF';
     const data = [2100, 3200, 3200, 2400, 2400, 1800, 1800, 2400, 2400, 3200, 3200, 3000, 3000, 3250, 3250];
     const categories = [
       '10AM',
@@ -101,10 +101,41 @@ export class NftChartCardComponent implements OnInit, OnDestroy {
 
     effect(() => {
       /** change chart theme */
+      let primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary');
+      primaryColor = this.HSLToHex(primaryColor);
+      console.log(primaryColor);
       this.chartOptions.tooltip = {
         theme: this.themeService.themeChanged(),
       };
+      this.chartOptions.colors = [primaryColor];
+      this.chartOptions.stroke!.colors = [primaryColor];
+      this.chartOptions.xaxis!.crosshairs!.stroke!.color = primaryColor;
     });
+  }
+
+  private HSLToHex(color: string): string {
+    const colorArray = color.split('%').join('').split(' ');
+    const colorHSL = colorArray.map(Number);
+    const hsl = {
+      h: colorHSL[0],
+      s: colorHSL[1],
+      l: colorHSL[2],
+    };
+
+    const { h, s, l } = hsl;
+
+    const hDecimal = l / 100;
+    const a = (s * Math.min(hDecimal, 1 - hDecimal)) / 100;
+    const f = (n: number) => {
+      const k = (n + h / 30) % 12;
+      const color = hDecimal - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+
+      // Convert to Hex and prefix with "0" if required
+      return Math.round(255 * color)
+        .toString(16)
+        .padStart(2, '0');
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
   }
 
   ngOnInit(): void {}
