8084dcd47be6dd9d0f28f102a63dd8cb903bc1d7
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2024-03-08 16:25:56 -0300

    test: add sidebar e2e

---
+++ tests-e2e/sidebar.e2e.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('check sidebar menu link', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+  await page.getByRole('navigation').locator('div').filter({ hasText: 'Auth' }).nth(3).click();
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await expect(page.locator('h2')).toContainText('Sign Up !');
+});
+
+test('check toggle sidebar', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+  const sidebar = page.locator('app-sidebar');
+  await page.getByRole('navigation').getByRole('button').first().click();
+  await page.waitForTimeout(500);
+  const collapsedSidebarSizes = await sidebar.boundingBox();
+  expect(collapsedSidebarSizes?.width).toBe(70);
+  await page.getByRole('navigation').getByRole('button').click();
+  await page.waitForTimeout(500);
+  const normalSidebarSizes = await sidebar.boundingBox();
+  expect(normalSidebarSizes?.width).toBe(256);
+});
