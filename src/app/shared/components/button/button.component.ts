ba1f6044987c37b178d313e12b7f4ecd9b58cc45
Luciano Oliveira <12519008+luciano-work@users.noreply.github.com>
2024-05-03 14:55:13 -0300

    refactor: add shadown property

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ type ButtonProps = {
   size: 'small' | 'medium' | 'large';
   shape: 'square' | 'rounded' | 'pill';
   tone: 'primary' | 'danger' | 'success' | 'warning' | 'info' | 'light';
+  shadow: 'none' | 'small' | 'medium' | 'large';
 };
 
 @Component({
@@ -21,6 +22,7 @@ export class ButtonComponent implements OnInit {
   size = input<ButtonProps['size']>('medium');
   shape = input<ButtonProps['shape']>('rounded');
   tone = input<ButtonProps['tone']>('primary');
+  shadow = input<ButtonProps['shadow']>('none');
   full = input(false, {
     transform: (value: boolean | string) => (typeof value === 'string' ? value === '' : value),
   });
@@ -34,32 +36,32 @@ export class ButtonComponent implements OnInit {
 
   impactClasses: Record<ButtonProps['tone'], Record<ButtonProps['impact'], string>> = {
     primary: {
-      bold: 'bg-primary text-primary-foreground shadow-md hover:bg-primary/90 focus-visible:ring-primary',
+      bold: 'bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:ring-primary',
       light: 'bg-primary/20 text-primary hover:bg-primary/30 focus-visible:ring-primary',
       none: 'bg-transparent text-primary hover:bg-primary/10 focus-visible:ring-primary',
     },
     danger: {
-      bold: 'bg-destructive text-white shadow-md hover:bg-destructive/90 focus-visible:ring-destructive',
+      bold: 'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive',
       light: 'bg-destructive/20 text-destructive hover:bg-destructive/30 focus-visible:ring-destructive',
       none: 'bg-transparent text-destructive hover:bg-destructive/10 focus-visible:ring-destructive',
     },
     success: {
-      bold: 'bg-green-500 text-green-950 shadow-md hover:bg-green-600 focus-visible:ring-green-500',
+      bold: 'bg-green-500 text-green-950 hover:bg-green-600 focus-visible:ring-green-500',
       light: 'bg-green-500/20 text-green-600 hover:bg-green-500/30 focus-visible:ring-green-500',
       none: 'bg-transparent text-green-600 hover:bg-green-500/10 focus-visible:ring-green-500',
     },
     warning: {
-      bold: 'bg-yellow-500 text-yellow-950 shadow-md hover:bg-yellow-600 focus-visible:ring-yellow-500',
+      bold: 'bg-yellow-500 text-yellow-950 hover:bg-yellow-600 focus-visible:ring-yellow-500',
       light: 'bg-yellow-500/20 text-yellow-600 hover:bg-yellow-500/30 focus-visible:ring-yellow-500',
       none: 'bg-transparent text-yellow-600 hover:bg-yellow-500/10 focus-visible:ring-yellow-500',
     },
     info: {
-      bold: 'bg-violet-500 text-white shadow-md hover:bg-violet-600 focus-visible:ring-violet-500',
+      bold: 'bg-violet-500 text-white hover:bg-violet-600 focus-visible:ring-violet-500',
       light: 'bg-violet-500/20 text-violet-600 hover:bg-violet-500/30 focus-visible:ring-violet-500',
       none: 'bg-transparent text-violet-600 hover:bg-violet-500/10 focus-visible:ring-violet-500',
     },
     light: {
-      bold: 'bg-muted text-muted-foreground shadow-md hover:bg-muted/90 focus-visible:ring-muted',
+      bold: 'bg-muted text-muted-foreground hover:bg-muted/90 focus-visible:ring-muted',
       light: 'bg-muted/20 text-muted-foreground hover:bg-muted focus-visible:ring-muted',
       none: 'bg-transparent text-muted-foreground hover:bg-muted focus-visible:ring-muted',
     },
@@ -77,6 +79,13 @@ export class ButtonComponent implements OnInit {
     pill: 'rounded-full',
   };
 
+  shadowClasses: Record<ButtonProps['shadow'], string> = {
+    none: '',
+    small: 'shadow-sm',
+    medium: 'shadow-md',
+    large: 'shadow-lg',
+  };
+
   constructor() {}
 
   ngOnInit(): void {
@@ -85,6 +94,7 @@ export class ButtonComponent implements OnInit {
       this.impactClasses[this.tone()][this.impact()],
       this.sizeClasses[this.size()],
       this.shapeClasses[this.shape()],
+      this.shadowClasses[this.shadow()],
       this.full() ? 'w-full' : '',
     );
   }
